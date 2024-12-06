35758165a10ad6bbdb8a364d7686787a806d220c
Alessia Tang <alessia.tang@example.com>
2024-12-05 17:41:27 -0800

    code review 1st

--- mosnopro_us/map_builder.py
+++ mosnopro_us/map_builder.py
@@ -1,26 +1,31 @@
 import folium
 import data_manager
 
+
 def plot_map():
     snotels = data_manager.load_snotel_points()
-    sites = ['Morse Lake', 'Olallie Meadows', 'Paradise', 'Stampede Pass', 'Stevens Pass', 'Swift Creek', 'Waterhole', 'Wells Creek', 'White Pass']
+    sites = ['Morse Lake', 'Olallie Meadows', 'Paradise',
+             'Stampede Pass', 'Stevens Pass', 'Swift Creek',
+             'Waterhole', 'Wells Creek', 'White Pass']
     snotels = snotels[snotels['name'].isin(sites)]
-    snotels['alt'] = (snotels['geometry'].z/3.28).astype(int).astype(str) + ' m elevation'
+    snotels['alt'] = (snotels['geometry']
+                      .z/3.28).astype(int).astype(str) + ' m elevation'
     wa_state = data_manager.load_washington_boundary()
 
     # plot these points on a folium map
-    m = folium.Map([wa_state.centroid.y.loc[0], wa_state.centroid.x.loc[0]], zoom_start=7, tiles="openstreetmap")
+    m = folium.Map([wa_state.centroid.y.loc[0], wa_state.centroid.x.loc[0]],
+                   zoom_start=7, tiles="openstreetmap")
 
     folium.GeoJson(wa_state.geometry).add_to(m)
     folium.GeoJson(snotels,
-                    control = False,
-                    marker = folium.CircleMarker(radius = 10,
-                                            weight = 0, #outline weight
-                                            fill_color = '#000000', 
-                                            fill_opacity = 1),
-                    tooltip=folium.GeoJsonTooltip(fields=["name", "id", "alt"]),
-                    popup=folium.GeoJsonPopup(fields=["name", "id", "alt"]),
-                    highlight_function=lambda x: {"fillOpacity": 0.8},
-                    zoom_on_click=True,
-                ).add_to(m)
+                   control=False,
+                   marker=folium.CircleMarker(radius=10,
+                                              weight=0,  # outline weight
+                                              fill_color='#000000',
+                                              fill_opacity=1),
+                   tooltip=folium.GeoJsonTooltip(fields=["name", "id", "alt"]),
+                   popup=folium.GeoJsonPopup(fields=["name", "id", "alt"]),
+                   highlight_function=lambda x: {"fillOpacity": 0.8},
+                   zoom_on_click=True,
+                   ).add_to(m)
     return m
--- mosnopro_us/plotting.py
+++ mosnopro_us/plotting.py
@@ -10,8 +10,8 @@ import pandas as pd
 
 
 def plot_layers(var, depth, ax=None, colormap='viridis', plot_soil=True,
-           plot_snow=True, variable_range=None, add_colorbar=True,
-           line_kwargs={}, cbar_kwargs={}):
+                plot_snow=True, variable_range=None, add_colorbar=True,
+                line_kwargs={}, cbar_kwargs={}):
     """
     Makes a layers plot. Example usage:
 
@@ -59,9 +59,9 @@ def plot_layers(var, depth, ax=None, colormap='viridis', plot_soil=True,
     lo_depth = depth.where(depth > 0).T
     hi_depth = depth.where(depth < 0).T
     if plot_soil and not plot_snow:
-        var = var.where((depth > 0).values[:,:-1]).T
+        var = var.where((depth > 0).values[:, :-1]).T
     elif plot_snow and not plot_soil:
-        var = var.where((depth < 0).values[:,:-1]).T
+        var = var.where((depth < 0).values[:, :-1]).T
     else:
         var = var.T
     time = depth.time.values
@@ -77,29 +77,30 @@ def plot_layers(var, depth, ax=None, colormap='viridis', plot_soil=True,
 
     # Create axes if needed
     if not ax:
-        fig, ax = plt.subplots(figsize=(18,8))
+        fig, ax = plt.subplots(figsize=(18, 8))
 
     # Plot soil layers - need to reverse because we plot bottom down
     if plot_soil:
-        for l in lo_depth.ifcToto.values[:-1][::-1]:
-            y = lo_depth[l]
+        for llo in lo_depth.ifcToto.values[:-1][::-1]:
+            y = lo_depth[llo]
             y[np.isnan(y)] = 0
-            ax.vlines(time, ymin=-y, ymax=0, color=rgba[l], **line_kwargs)
+            ax.vlines(time, ymin=-y, ymax=0, color=rgba[llo], **line_kwargs)
 
     # Plot snow layers - plot top down
     if plot_snow:
-        for l in hi_depth.ifcToto.values[:-1]:
-            y = hi_depth[l]
+        for lhi in hi_depth.ifcToto.values[:-1]:
+            y = hi_depth[lhi]
             y[np.isnan(y)] = 0
             if (y != 0).any():
-                ax.vlines(time, ymin=0, ymax=-y, color=rgba[l], **line_kwargs)
+                ax.vlines(time, ymin=0, ymax=-y,
+                          color=rgba[lhi], **line_kwargs)
 
     # Add the colorbar
     mappable = cm.ScalarMappable(norm=norm, cmap=cmap)
     mappable.set_array(var.values.flatten())
     try:
         label = var.long_name
-    except:
+    except Exception:
         label = var.name
     if 'label' not in cbar_kwargs.keys():
         cbar_kwargs['label'] = label
@@ -109,62 +110,64 @@ def plot_layers(var, depth, ax=None, colormap='viridis', plot_soil=True,
         plt.gcf().colorbar(mappable, **cbar_kwargs)
     return ax, mappable
 
+
 # Create colomaps
 
 # The range of temperature bins in Fahrenheit
-a = np.arange(-20,5,1)
+a = np.arange(-20, 5, 1)
 
 # Bins normalized between 0 and 1
 norm = [(float(i)-min(a))/(max(a)-min(a)) for i in a]
 
 # Color tuple for every bin
-C = np.array([[145,0,63],
-              [206,18,86],
-              [231,41,138],
-              [223,101,176],
-              [255,115,223],
-              [255,190,232],
-              [255,255,255],
-              [218,218,235],
-              [188,189,220],
-              [158,154,200],
-              [117,107,177],
-              [84,39,143],
-              [13,0,125],
-              [13,61,156],
-              [0,102,194],
-              [41,158,255], 
-              [74,199,255], 
-              [115,215,255], 
-              [173,255,255],
-              [48,207,194], 
-              [0,153,150], 
-              [18,87,87],
-              [6,109,44],
-              [49,163,84],
-              [116,196,118],
-              [161,217,155],
-              [211,255,190],  
-              [255,255,179], 
-              [255,237,160], 
-              [254,209,118], 
-              [254,174,42], 
-              [253,141,60], 
-              [252,78,42], 
-              [227,26,28], 
-              [177,0,38], 
-              [128,0,38], 
-              [89,0,66], 
-              [40,0,40]])/255.
+C = np.array([[145, 0, 63],
+              [206, 18, 86],
+              [231, 41, 138],
+              [223, 101, 176],
+              [255, 115, 223],
+              [255, 190, 232],
+              [255, 255, 255],
+              [218, 218, 235],
+              [188, 189, 220],
+              [158, 154, 200],
+              [117, 107, 177],
+              [84, 39, 143],
+              [13, 0, 125],
+              [13, 61, 156],
+              [0, 102, 194],
+              [41, 158, 255],
+              [74, 199, 255],
+              [115, 215, 255],
+              [173, 255, 255],
+              [48, 207, 194],
+              [0, 153, 150],
+              [18, 87, 87],
+              [6, 109, 44],
+              [49, 163, 84],
+              [116, 196, 118],
+              [161, 217, 155],
+              [211, 255, 190],
+              [255, 255, 179],
+              [255, 237, 160],
+              [254, 209, 118],
+              [254, 174, 42],
+              [253, 141, 60],
+              [252, 78, 42],
+              [227, 26, 28],
+              [177, 0, 38],
+              [128, 0, 38],
+              [89, 0, 66],
+              [40, 0, 40]])/255.
 
-# Create a tuple for every color indicating the normalized position on the colormap and the assigned color.
+# Create a tuple for every color indicating the normalized position
+# on the colormap and the assigned color.
 COLORS = []
 for i, n in enumerate(norm):
     COLORS.append((n, C[i]))
 
 temp_cmap = colors.LinearSegmentedColormap.from_list("Temperature", COLORS)
 # The sky covered by clouds in percent (%)
-a = range(50,500,50)
+a = range(50, 500, 50)
 
 # Normalize the bin between 0 and 1 (uneven bins are important here)
 norm = [(float(i)-min(a))/(max(a)-min(a)) for i in a]
@@ -181,7 +184,8 @@ C = np.array([[36, 160, 242],
               [110, 110, 110],
               [80, 80, 80]])
 
-# Create a tuple for every color indicating the normalized position on the colormap and the assigned color.
+# Create a tuple for every color indicating the normalized position
+# on the colormap and the assigned color.
 COLORS = []
 COLORS_r = []
 for i, n in enumerate(norm):
@@ -192,12 +196,15 @@ for i, n in enumerate(norm):
 density_cmap = colors.LinearSegmentedColormap.from_list("density", COLORS)
 
 # create the inverse
-density_cmap_r = colors.LinearSegmentedColormap.from_list("density_r", COLORS_r)
+density_cmap_r = colors.LinearSegmentedColormap.from_list(
+    "density_r", COLORS_r)
+
 
 def get_snotel_depth(df, min_time):
-    """This function gets the observed snow depth from the snotel and returns it as a pandas series"""
+    """This function gets the observed snow depth
+    from the snotel and returns it as a pandas series"""
     # get snotel observation depth
-    snow_depth_obs = df.loc[min_time:]['SNOWDEPTH'].resample('1D').max().shift()  * 2.54/100
+    snow_depth_obs = df.loc[min_time:]['SNOWDEPTH'].resample('1D').max().shift() * 2.54/100
     # replace below zero values with 0
     snow_depth_obs[snow_depth_obs < 0] = 0
     # replace nan values with nan
@@ -206,6 +213,7 @@ def get_snotel_depth(df, min_time):
     snow_depth_obs = snow_depth_obs.interpolate()
     return snow_depth_obs
 
+
 def produce_temp_depth_fig(summa_df, snotel_df, name):
     # get temperature and depth layers
     # get depth of each layer
@@ -213,21 +221,39 @@ def produce_temp_depth_fig(summa_df, snotel_df, name):
     # get temperature of each layer
     temp = summa_df.isel(hru=0)['mLayerTemp']
     # convert to Celsius
-    temp = temp.where(temp<-100, temp-273.15)
+    temp = temp.where(temp < -100, temp-273.15)
 
     min_time = pd.to_datetime(depth.time.values.min())
     # get observed snow depth
     snow_depth_obs = get_snotel_depth(snotel_df, min_time)
 
-    fig, ax = plt.subplots(figsize=(8,5))
-    plot_layers(temp, (depth), colormap=temp_cmap, plot_soil=False, plot_snow=True, cbar_kwargs={'label': 'Temperature (C)', 'ticks':np.arange(-10,1,1)}, variable_range=[-10, 1], ax=ax)
-    (summa_df['scalarSnowDepth']).plot(color='k', linewidth=2, ax=ax, label='SUMMA Modeled Snow Depth');
-    snow_depth_obs.plot(color='red', ls='--', linewidth=2, ax=ax, label='Observed Snow Depth');
+    fig, ax = plt.subplots(figsize=(8, 5))
+    plot_layers(temp, (depth),
+                colormap=temp_cmap,
+                plot_soil=False,
+                plot_snow=True,
+                cbar_kwargs={'label': 'Temperature (C)',
+                             'ticks': np.arange(-10, 1, 1)},
+                variable_range=[-10, 1],
+                ax=ax)
+    (summa_df['scalarSnowDepth']).plot(color='k',
+                                       linewidth=2,
+                                       ax=ax,
+                                       label='SUMMA Modeled Snow Depth')
+    snow_depth_obs.plot(color='red',
+                        ls='--',
+                        linewidth=2,
+                        ax=ax,
+                        label='Observed Snow Depth')
     ax.legend(loc='upper left')
-    ax.set_title(f"{name} Snow Depth\n{snow_depth_obs.index.min().strftime('%Y-%m-%d %H:%M')} to {snow_depth_obs.index.max().strftime('%Y-%m-%d %H:%M')}\nElevation: {snotel_df['geometry'].iloc[0][-5:-1]} ft")
+    start = snow_depth_obs.index.min().strftime('%Y-%m-%d %H:%M')
+    end = snow_depth_obs.index.max().strftime('%Y-%m-%d %H:%M')
+    elevation = snotel_df['geometry'].iloc[0][-5:-1]
+    ax.set_title(f"{name} Snow Depth\n{start} to {end}\nElevation: {elevation} ft")
     ax.set_ylabel('Snow Depth (m)')
     ax.set_xlabel('Date')
     return fig
+
 
 def produce_density_depth_fig(summa_df, snotel_df, name):
     # get temperature and depth layers
@@ -235,21 +261,39 @@ def produce_density_depth_fig(summa_df, snotel_df, name):
     depth = summa_df.isel(hru=0)['iLayerHeight']
     # get density of each layer
     density = summa_df.isel(hru=0)['mLayerVolFracWat']*1000
-    density = density.where(density>=0, np.nan)
+    density = density.where(density >= 0, np.nan)
 
     min_time = depth.time.values.min()
     # get observed snow depth
     snow_depth_obs = get_snotel_depth(snotel_df, min_time)
 
-    fig, ax = plt.subplots(figsize=(8,5))
-    plot_layers(density, (depth), colormap=density_cmap_r, plot_soil=False, plot_snow=True, cbar_kwargs={'label': 'Density (kg/m$^3$)', 'ticks':np.arange(100,400,30)}, variable_range=[100, 400], ax=ax)
-    (summa_df['scalarSnowDepth']).plot(color='k', linewidth=2, ax=ax, label='SUMMA Modeled Snow Depth');
-    snow_depth_obs.plot(color='red', ls='--', linewidth=2, ax=ax, label='Observed Snow Depth');
+    fig, ax = plt.subplots(figsize=(8, 5))
+    plot_layers(density, (depth),
+                colormap=density_cmap_r,
+                plot_soil=False,
+                plot_snow=True,
+                cbar_kwargs={'label': 'Density (kg/m$^3$)',
+                             'ticks': np.arange(100, 400, 30)},
+                variable_range=[100, 400],
+                ax=ax)
+    (summa_df['scalarSnowDepth']).plot(color='k',
+                                       linewidth=2,
+                                       ax=ax,
+                                       label='SUMMA Modeled Snow Depth')
+    snow_depth_obs.plot(color='red',
+                        ls='--',
+                        linewidth=2,
+                        ax=ax,
+                        label='Observed Snow Depth')
     ax.legend(loc='upper left')
-    ax.set_title(f"{name} Snow Depth\n{snow_depth_obs.index.min().strftime('%Y-%m-%d %H:%M')} to {snow_depth_obs.index.max().strftime('%Y-%m-%d %H:%M')}\nElevation: {snotel_df['geometry'].iloc[0][-5:-1]} ft")
+    start = snow_depth_obs.index.min().strftime('%Y-%m-%d %H:%M')
+    end = snow_depth_obs.index.max().strftime('%Y-%m-%d %H:%M')
+    elevation = snotel_df['geometry'].iloc[0][-5:-1]
+    ax.set_title(f"{name} Snow Depth\n{start} to {end}\nElevation: {elevation} ft")
     ax.set_ylabel('Snow Depth (m)')
     ax.set_xlabel('Date')
     return fig
+
 
 def produce_liquid_water_depth_fig(summa_df, snotel_df, name):
     # get temperature and depth layers
@@ -257,17 +301,33 @@ def produce_liquid_water_depth_fig(summa_df, snotel_df, name):
     depth = summa_df.isel(hru=0)['iLayerHeight']
     # get density of each layer
     liq_water = summa_df.isel(hru=0)['mLayerVolFracLiq']*1000
-    liq_water = liq_water.where(liq_water>=0, np.nan)
+    liq_water = liq_water.where(liq_water >= 0, np.nan)
 
     # get observed snow depth
     snow_depth_obs = get_snotel_depth(snotel_df)
 
-    fig, ax = plt.subplots(figsize=(8,5))
-    plot_layers(liq_water, (depth), colormap=density_cmap_r, plot_soil=False, plot_snow=True, cbar_kwargs={'label': 'Liquid Water Content (kg/m$^3$)', 'ticks':np.arange(0,100,10)}, variable_range=[0, 100], ax=ax)
-    (summa_df['scalarSnowDepth']).plot(color='k', linewidth=2, ax=ax, label='SUMMA Modeled Snow Depth');
-    snow_depth_obs.plot(color='red', ls='--', linewidth=2, ax=ax, label='Observed Snow Depth');
+    fig, ax = plt.subplots(figsize=(8, 5))
+    plot_layers(liq_water, (depth),
+                colormap=density_cmap_r,
+                plot_soil=False,
+                plot_snow=True,
+                cbar_kwargs={'label': 'Liquid Water Content (kg/m$^3$)',
+                             'ticks': np.arange(0, 100, 10)},
+                variable_range=[0, 100], ax=ax)
+    (summa_df['scalarSnowDepth']).plot(color='k',
+                                       linewidth=2,
+                                       ax=ax,
+                                       label='SUMMA Modeled Snow Depth')
+    snow_depth_obs.plot(color='red',
+                        ls='--',
+                        linewidth=2,
+                        ax=ax,
+                        label='Observed Snow Depth')
     ax.legend(loc='upper left')
-    ax.set_title(f"{name} Snow Depth\n{snow_depth_obs.index.min().strftime('%Y-%m-%d %H:%M')} to {snow_depth_obs.index.max().strftime('%Y-%m-%d %H:%M')}\nElevation: {snotel_df['geometry'].iloc[0][-5:-1]} ft")
+    start = snow_depth_obs.index.min().strftime('%Y-%m-%d %H:%M')
+    end = snow_depth_obs.index.max().strftime('%Y-%m-%d %H:%M')
+    elevation = snotel_df['geometry'].iloc[0][-5:-1]
+    ax.set_title(f"{name} Snow Depth\n{start} to {end}\nElevation: {elevation} ft")
     ax.set_ylabel('Snow Depth (m)')
     ax.set_xlabel('Date')
     return fig
